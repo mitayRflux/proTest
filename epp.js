f06214dc39803e88de9ab1f2aa94c81e90c42ba5
mitayRflux <daflux2@gmail.com>
2022-09-12 22:12:39 +0300

    here starts chages

--- epp.js
+++ epp.js
@@ -1,1 +1,10 @@
 //alert('Hi Users!');
+const main = document.querySelector('#main');
+const screens = document.querySelectorAll('.place');
+
+main.addEventListener('click',(event)=>{
+  event.preventDefault();
+}
+);
+
+//console.log(screens);
