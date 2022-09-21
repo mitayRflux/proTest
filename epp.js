d4757f1f1953c4cc796c18c652c2945cc23ae437
mitayRflux <daflux2@gmail.com>
2022-09-21 23:19:06 +0300

    new version

--- epp.js
+++ epp.js
@@ -1,6 +1,8 @@
 //alert('Hi Users!');
 
 const start = document.querySelector('#start');
+const btn = document.querySelector('#btn');
+const enter = document.querySelector('#entr');
 const screens = document.querySelectorAll('.screen');
 
 //console.log(screens);
@@ -9,3 +11,15 @@ start.addEventListener('click',(event)=>{
   event.preventDefault();
   screens[0].classList.add('up');
 });
+
+btn.addEventListener('click',(event)=>{
+screens[1].classList.add('up');
+});
+
+enter.addEventListener('click',(event)=>{
+  event.preventDefault();
+  screens[2].classList.add('up');
+});
+
+
+//setTimeout(()=>{location.reload();},2000);
