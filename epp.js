1632fc253adfd6d8e069234088b3bd11ecdf1e1e
mitayRflux <daflux2@gmail.com>
2022-09-21 21:56:42 +0300

    проба№1

--- epp.js
+++ epp.js
@@ -1,45 +1,11 @@
 //alert('Hi Users!');
 
+const start = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
 
-const upBtn = document.querySelector('.up-btn');
-const downBtn = document.querySelector('.down-btn');
-const sidebar = document.querySelector('.sitebar');
-const mainSlide = document.querySelector('.main-menu');
-const container = document.querySelector('#section');
-//console.log(downBtn);
-const slidesCount = mainSlide.querySelectorAll('div').length;
+//console.log(screens);
 
-let activeSlideIndex = 0;
-
-sidebar.style.top = `-${(slidesCount-1)*100}vh`;
-
-upBtn.addEventListener('click',function(){
-changeSlide('up');
+start.addEventListener('click',(event)=>{
+  event.preventDefault();
+  screens[0].classList.add('up');
 });
-
-downBtn.addEventListener('click',function(){
-  changeSlide('down');
-});
-
-function changeSlide(direction){
-  if(direction===0){
-   activeSlideIndex++;
-   if(activeSlideIndex===slidesCount){
-     activeSlideIndex=0;
-   }
-  }else if(direction ==='down'){
-    activeSlideIndex--;
-    if(activeSlideIndex < 0){
-      activeSlideIndex = slidesCount-1;
-    }
-  }
-
-  const height = container.clientHeight;
-
-  mainSlide.style.transform = 
-  `translateY(-${activeSlideIndex*height}px)`;
-
-  sidebar.style.transform = 
-  `translateY(${activeSlideIndex*height}px)`;
-
-}
